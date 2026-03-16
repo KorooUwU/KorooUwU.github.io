47d3e754616ab7502e3a5f76d344f42476c7e97e
Koroo <hoanganh05012005@gmail.com>
2026-03-16 10:40:01 +0700

    fix

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ResumeIcon from '@mui/icons-material/Description';
 import '../assets/styles/Main.scss';
 
 function Main() {
@@ -9,19 +10,19 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://avatars.alphacoders.com/avatars/view/375159" alt="Avatar" />
+          <img src="src/assets/images/avatar.jpg" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/KorooUwU" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="koroo.pdf" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="koroo.pdf" target="_blank" rel="noreferrer"><ResumeIcon/></a>
           </div>
           <h1>Trương Phúc Hoàng Anh</h1>
-          <p>FIT Student | Frontend Developer</p>
+          <p>IT Student | Frontend Developer</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/KorooUwU" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="koroo.pdf" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="koroo.pdf" target="_blank" rel="noreferrer"><ResumeIcon/></a>
           </div>
         </div>
       </div>
